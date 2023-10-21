4ef0cc47c72dc5e967d63fa23312e10228c012ed
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-10-21 21:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //your JS code here. If required.
 const button =document.getElementById("enterBtn");
 const para =document.getElementById("status");
-
+let h1Element = document.createEvent("h1");
+h1Element.innerText =“Entered Metaverse”;
+h1Element.id="status";
+let bodyTag = document.querySelector(".container");
 button.addEventListener("click",()=>{
-	para.innerText="Entered Mataverse";
+	bodyTag.insertBefore(h1Element,para);
+	para.remove();
+	
 });
